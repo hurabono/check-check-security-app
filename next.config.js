de9b02b1828e021ece8434845dd0a22bf80a6e45
Heesu Cho <101083889@georgebrown.ca>
2025-09-25 12:26:58 -0400

    web build

--- next.config.js
+++ next.config.js
@@ -1,17 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: false, 
   transpilePackages: [
     "expo",
     "expo-router",
+    "expo-modules-core",
+    "expo-linking",
+    "expo-web-browser",
     "react-native",
     "react-native-web",
+    "react-native-safe-area-context",
     "@clerk/clerk-expo"
   ],
   webpack: (config) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
-      "react-native$": "react-native-web",   // RN → RN Web
+      "react-native$": "react-native-web",
     };
     return config;
   },
