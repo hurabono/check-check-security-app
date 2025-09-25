0c4d9e1c65ead8a6509532e2d43b71842f88432f
Heesu Cho <101083889@georgebrown.ca>
2025-09-25 12:17:40 -0400

    web build

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,12 @@ import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
 
 export default function HomePage() {
-  const { isSignedIn } = useAuth();
+  const { isSignedIn, isLoaded } = useAuth();
+  if (!isLoaded) return null;
 
-  if (!isSignedIn) {
-    // 로그인 안된 경우 → 로그인 페이지로
-    return <Redirect href="/(auth)/sign-in" />;
-  }
-
-  // 로그인된 경우 → index.jsx 페이지로
-  return <Redirect href="/sign-in" />;
+  return (
+    <Redirect
+      href={isSignedIn ? "/(tabs)" : "/(auth)/sign-in"}
+    />
+  );
 }
